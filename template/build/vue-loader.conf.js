21a1480159a72e55edb11eeb477a3e6e61383773
liwb <liwb@192.168.0.104>
2017-11-01 21:35:49 +0800

    [build] 1.1.3

--- template/build/vue-loader.conf.js
+++ template/build/vue-loader.conf.js
@@ -5,9 +5,18 @@ const isProduction = utils.isProduction()
 const pxtorem = require('postcss-pxtorem')
 
 module.exports = {
+  loaders: utils.cssLoaders({
+    sourceMap: isProduction
+      ? config.build.productionSourceMap
+      : config.dev.cssSourceMap,
+    extract: isProduction
+  }),
   // 解决把图片提前 require 传给一个变量再传给组件
   transformToRequire: {
-    avatar: ['default-src']
+    video: 'src',
+    source: 'src',
+    img: 'src',
+    image: 'xlink:href'
   },
   // 为了去掉元素间的空格
   preserveWhitespace: false,
@@ -19,15 +28,5 @@ module.exports = {
     replace: true,
     mediaQuery: false,
     minPixelValue: 0
-  })],
-  autoprefixer: {
-    browsers: ["Android >= 2.3", "iOS >= 4"], //, "ChromeAndroid > 1%"
-    cascade: false // 不美化输出 css
-  },
-  loaders: utils.cssLoaders({
-    sourceMap: isProduction
-      ? config.build.productionSourceMap
-      : config.dev.cssSourceMap,
-    extract: isProduction
-  })
+  })]
 }
